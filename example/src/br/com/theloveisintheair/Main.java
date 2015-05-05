5484e3da30fae327d24cebe1eb319bf627e9495e
Maiara <maiaraoliveira.dejesus@gmail.com>
2015-05-05 11:11:17 -0300

    First Commit
    Projeto usando system out como interface

---
+++ example/src/br/com/theloveisintheair/Main.java
@@ -0,0 +1,15 @@
+package br.com.theloveisintheair;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if(args != null && args.length > 0){
+			if(args[0].equalsIgnoreCase("Maiara")){
+				System.out.println("EU te amo demais =D");
+			}else{
+				System.out.println("Eu só amo a mai mai [=");
+			}
+		}
+	}
+
+}
---
+++ rank-match-cmn/src/br/com/rankmatch/util/Utils.java
@@ -0,0 +1,8 @@
+package br.com.rankmatch.util;
+
+
+public class Utils {
+	
+	
+	
+}
